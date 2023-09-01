ce2d43758b3fc3f9d9bc65e8a38b96ac24e7f89c
kamilaibek <taalakamilaibekkuzu@gmail.com>
2023-09-01 18:59:19 +0600

    django1

--- tasks/views.py
+++ tasks/views.py
@@ -5,7 +5,7 @@ from tasks.models import Task
 
 def get_list(request):
     if request.method == 'GET':
-        tasks = Task.objects.all()
+        tasks = Task.objects.filter(is_active=True).order_by('-id')
         return render(request, 'index.html', {"tasks": tasks})
 
 
@@ -26,7 +26,7 @@ def update_task(request, id):
         description = request.POST.get('description')
         created_at = request.POST.get('created_at')
         type = request.POST.get('type')
-        task = Task.objects.get(id=id)
+        task = Task.objects.filter(id=id, is_active=True).first()
         task.title = title
         task.description = description
         task.created_at = created_at
@@ -34,15 +34,16 @@ def update_task(request, id):
         task.save()
         return redirect('index')
     context = {
-        'data': Task.objects.get(id=id)
+        'data': Task.objects.filter(id=id, is_active=True).first()
     }
     return render(request, 'update.html', context)
 
 
 def delete_task(request, id):
     if request.method == 'POST':
-        task = Task.objects.get(id=id)
-        task.delete()
+        task = Task.objects.get(id=id, is_active=True)
+        task.is_active = False
+        task.save()
         return redirect('index')
     return render(request, 'index.html')
 
